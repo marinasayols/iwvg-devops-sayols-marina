d1b6f5328e2975ef5676bbcfa2399ff98bec37b1
MarinaSayols <marina.sayols@alumnos.upm.es>
2021-09-29 20:10:26 +0200

    Fix formatting #11

--- src/main/java/es/upm/miw/iwvg_devops/code/Searches.java
+++ src/main/java/es/upm/miw/iwvg_devops/code/Searches.java
@@ -4,14 +4,14 @@ import java.util.stream.Stream;
 
 public class Searches {
 
-    public Stream<Double> findDecimalFractionByNegativeSignFraction(){
+    public Stream<Double> findDecimalFractionByNegativeSignFraction() {
         return new UsersDatabase().findAll()
                 .flatMap(user -> user.getFractions().stream())
                 .filter(Fraction::isNegative)
                 .map(Fraction::decimal);
     }
 
-    public Fraction findHighestFraction(){
+    public Fraction findHighestFraction() {
         return new UsersDatabase().findAll()
                 .flatMap(user -> user.getFractions().stream())
                 .max(Fraction::compare)
