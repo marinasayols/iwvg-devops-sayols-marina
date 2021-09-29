a8df003137b5af8c81e9b51b8cd6daf06b74b6c4
MarinaSayols <marina.sayols@alumnos.upm.es>
2021-09-29 19:37:29 +0200

    Fix code smells #10

--- src/test/java/es/upm/miw/iwvg_devops/code/UserTest.java
+++ src/test/java/es/upm/miw/iwvg_devops/code/UserTest.java
@@ -8,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.util.ArrayList;
 import java.util.List;
 
-public class UserTest {
+class UserTest {
 
     private User user;
 
