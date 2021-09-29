c2e5693cbc258a31d1fea70a773c748d9c31dc05
MarinaSayols <marina.sayols@alumnos.upm.es>
2021-09-29 19:27:57 +0200

    Add findDecimalFractionByNegativeSignFraction #10

--- src/main/java/es/upm/miw/iwvg_devops/code/Searches.java
+++ src/main/java/es/upm/miw/iwvg_devops/code/Searches.java
@@ -1,2 +1,13 @@
-package es.upm.miw.iwvg_devops.code;public class Searches {
+package es.upm.miw.iwvg_devops.code;
+
+import java.util.stream.Stream;
+
+public class Searches {
+
+    public Stream<Double> findDecimalFractionByNegativeSignFraction(){
+        return new UsersDatabase().findAll()
+                .flatMap(user -> user.getFractions().stream())
+                .filter(Fraction::isNegative)
+                .map(Fraction::decimal);
+    }
 }
